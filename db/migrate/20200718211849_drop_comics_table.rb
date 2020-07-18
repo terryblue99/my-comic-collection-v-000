839188ac05b080e85cd93c37cfbeb14a0aafd85c
terryblue99 <terryblue@comcast.net>
2020-07-18 16:25:52 -0500

    add last_worn column to  comics table

---
+++ db/migrate/20200718211849_drop_comics_table.rb
@@ -0,0 +1,9 @@
+class DropComicsTable < ActiveRecord::Migration[6.0]
+  def up
+    drop_table :comics
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_17_03_194351) do
+ActiveRecord::Schema.define(version: 2020_17_18_212559) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_17_03_194351) do
     t.string "comic_publisher"
     t.string "comic_number"
     t.string "year_published"
+    t.string "last_worn"
     t.decimal "cost", precision: 8, scale: 2, default: "0.0"
     t.string "notes"
     t.integer "user_id"
